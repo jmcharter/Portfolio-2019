93bc70ddecaedf484769c00e0e5c8f8dae3f9726
Jon Charter <jmcharter@gmail.com>
2019-12-31 08:18:34 +0000

    added new sectional divs, added smooth scrollTo for nav links

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,4 +1,20 @@
-//Get DOM Elements
+//Get Link DOM Elements
+const home_link = document.querySelector('#home-link');
+const projects_link = document.querySelector('#projects-link');
+const about_link = document.querySelector('#about-link');
+const contact_link = document.querySelector('#contact-link');
+
+//Get targets for links
+const home_target = document.querySelector('#home');
+const projects_target = document.querySelector('#projects');
+const about_target = document.querySelector('#about');
+const contact_target = document.querySelector('#contact');
+
+nav_links = [home_link, projects_link, about_link, contact_link];
+link_targets = [home_target, projects_target, about_target, contact_target];
+
+
+//Get Nav DOM Elements
 const menu_text = document.querySelector('.menu-text')
 const menu_btn = document.querySelector('.menu-btn');
 const btn_line = document.querySelectorAll('.btn-line');
@@ -7,10 +23,17 @@ const menu_nav = document.querySelector('.menu-nav')
 const brand = document.querySelector('.brand')
 const nav_items = document.querySelectorAll('.nav-item')
 
+for (let i = 0; i < nav_links.length; i++) {
+    let link = nav_links[i];
+    link.addEventListener('click', (event) => {
+        event.preventDefault();
+        toggle_menu();
+        link_targets[i].scrollIntoView({ behavior: 'smooth' });
+    });
+}
+
 //Initialize menu
 let show_menu = false;
-
-
 
 let toggle_menu = function () {
     if (!show_menu) {
@@ -34,4 +57,4 @@ let toggle_menu = function () {
     }
 }
 
-menu_btn.addEventListener('click', toggle_menu)
+menu_btn.addEventListener('click', toggle_menu);
